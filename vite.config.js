9f1312ddffa657b0362d910608c8f62af86ccbce
Diego <diegoneves2021123@gmail.com>
2026-02-09 06:40:06 -0300

    atualização

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'github'
-    ? '/TrindaBoost/'
-    : '/',
+  base: mode === 'github' ? '/TrindaBoost/' : '/',
   server: {
     port: 5173,
     open: true
-  }
+  },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'termos.html', // arquivo na raiz do projeto
+          dest: '.'           // copia para a raiz do dist
+        }
+      ]
+    })
+  ]
 }))
