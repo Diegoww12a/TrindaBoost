4e2ebc215ac4c598d3b692e0a312c308ffda461e
Diego <diegoneves2021123@gmail.com>
2026-02-09 06:54:28 -0300

    atualização

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
+  base: '/', // raiz do Netlify
   plugins: [
     viteStaticCopy({
       targets: [
         {
-          src: 'termos.html',  // arquivo na raiz do projeto
-          dest: '.'             // destino dentro de dist
+          src: 'termos.html', // arquivo na raiz do projeto
+          dest: '.'           // copia para a raiz do dist
         }
       ]
     })
-  ],
-  base: '/TrindaBoost/', // se for GitHub Pages
+  ]
 })
